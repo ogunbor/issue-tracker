f49663e522bcc5fd1caff5917191791e0b531241
mompei <ogunborjerry@gmail.com>
2023-10-31 11:00:20 +0100

    Fetch data with React Query

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,35 +1,42 @@
-'use client'; 
+"use client";
 
-import { User } from '@prisma/client';
-import { Select } from '@radix-ui/themes'
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import { User } from "@prisma/client";
+import { Select } from "@radix-ui/themes";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { Skeleton } from "@/app/components";
 
 const AssigneeSelect = () => {
-    const [users, setUsers] = useState<User[]>([]);
+  const {
+    data: users,
+    error,
+    isLoading,
+  } = useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: () => axios.get("/api/users").then((res) => res.data),
+    staleTime: 60 * 1000, //60s
+    retry: 3,
+  });
 
-    useEffect(() => {
-      const fetchUsers = async () => {
-        const {data} = await axios.get<User[]>('/api/users');
-        setUsers(data);
-      }
-  
-      fetchUsers();
-    }, [])
-  
+  if (isLoading) return <Skeleton />;
+  if (error) return null;
+
   return (
     <Select.Root>
-      <Select.Trigger placeholder='Assign...' />
+      <Select.Trigger placeholder="Assign..." />
       <Select.Content>
-         <Select.Group>
-            <Select.Label>Suggestions</Select.Label>
-            {users.map(user => (
-              <Select.Item key={user.id} value={user.id}>{user.name}</Select.Item>)
-            )}
-         </Select.Group>
+        <Select.Group>
+          <Select.Label>Suggestions</Select.Label>
+          {users?.map((user) => (
+            <Select.Item key={user.id} value={user.id}>
+              {user.name}
+            </Select.Item>
+          ))}
+        </Select.Group>
       </Select.Content>
     </Select.Root>
-  )
-}
+  );
+};
 
-export default AssigneeSelect
+export default AssigneeSelect;
