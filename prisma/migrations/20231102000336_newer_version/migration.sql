aa41adeeb0ff68bb6235f08d89311c0e5f412803
mompei <ogunborjerry@gmail.com>
2023-11-02 01:24:02 +0100

    migrations

--- prisma/migrations/20231102000336_newer_version/migration.sql
+++
@@ -1,8 +0,0 @@
--- RenameIndex
-ALTER TABLE `account` RENAME INDEX `Account_userId_fkey` TO `Account_userId_idx`;
-
--- RenameIndex
-ALTER TABLE `issue` RENAME INDEX `Issue_assignedToUserId_fkey` TO `Issue_assignedToUserId_idx`;
-
--- RenameIndex
-ALTER TABLE `session` RENAME INDEX `Session_userId_fkey` TO `Session_userId_idx`;
--- prisma/migrations/20231102002340_migrations/migration.sql
+++ prisma/migrations/20231102002340_migrations/migration.sql
@@ -8,6 +8,7 @@ CREATE TABLE `Issue` (
     `updatedAt` DATETIME(3) NOT NULL,
     `assignedToUserId` VARCHAR(255) NULL,
 
+    INDEX `Issue_assignedToUserId_idx`(`assignedToUserId`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -26,6 +27,7 @@ CREATE TABLE `Account` (
     `id_token` TEXT NULL,
     `session_state` VARCHAR(191) NULL,
 
+    INDEX `Account_userId_idx`(`userId`),
     UNIQUE INDEX `Account_provider_providerAccountId_key`(`provider`, `providerAccountId`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -38,6 +40,7 @@ CREATE TABLE `Session` (
     `expires` DATETIME(3) NOT NULL,
 
     UNIQUE INDEX `Session_sessionToken_key`(`sessionToken`),
+    INDEX `Session_userId_idx`(`userId`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -62,12 +65,3 @@ CREATE TABLE `VerificationToken` (
     UNIQUE INDEX `VerificationToken_token_key`(`token`),
     UNIQUE INDEX `VerificationToken_identifier_token_key`(`identifier`, `token`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- AddForeignKey
-ALTER TABLE `Issue` ADD CONSTRAINT `Issue_assignedToUserId_fkey` FOREIGN KEY (`assignedToUserId`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE `Account` ADD CONSTRAINT `Account_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE `Session` ADD CONSTRAINT `Session_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
